914fb127a9b269d91533e185112464c2cca777dc
AbdoulBaguiM <72513211+AbdoulBaguiM@users.noreply.github.com>
2024-06-22 17:32:38 +0100

    22-06-2024: Synced with backend widget

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./assets/css/index.css";
 import { createRoot } from "react-dom/client";
 
 const div = document.getElementById("agencyreviews360-widgets")!;
-const domain = "http://localhost:8000/api";
+const domain = "https://agencyreviews360.com/api";
 
 createRoot(div).render(
   <React.StrictMode>
